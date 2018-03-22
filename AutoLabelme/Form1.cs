809a65661bf70afd2e13879d713a80b2de1049d7
alisa <alisa.mao@vehicular.ai>
2018-03-22 19:42:06 +0800

    some Update to The size label Object

--- AutoLabelme/Form1.cs
+++ AutoLabelme/Form1.cs
@@ -23,8 +23,8 @@ namespace AutoLabelme
         /// <summary>
         /// //
         /// </summary>
-        string imgFoldPath = "D:\\ad_simulation_Logitech\\ad_simulation\\va_sim\\Drive_1_4_2\\signShots\\";//The Image Folder
-        string imgInfoPath = "D:\\ad_simulation_Logitech\\ad_simulation\\va_sim\\Drive_1_4_2\\img_info_folder\\ ";//The ImageTXT Folder
+        string imgFoldPath = @"D:\ad_simulation_Logitech\ad_simulation\va_sim\Drive_1_4_2\signShots\\";//The Image Folder
+        string imgInfoPath = @"D:\ad_simulation_Logitech\ad_simulation\va_sim\Drive_1_4_2\img_info_folder\\ ";//The ImageTXT Folder
 
         DirectoryInfo imgFold;
         DirectoryInfo imgInfoFold;
@@ -33,8 +33,8 @@ namespace AutoLabelme
 
         
         Image openImage;
-
-        int[] affirScale = { 205, 120, 91, 66, 55 };
+        //1.0 -1.9 ,   2.0-2.9 ,  3.0-3.9,   4.0-4.9      5.0-5.9, 
+        int[] affirScale = { 205, 180, 100, 80, 66 };//,      50  ,  40};
         //开始自动画图
         private void button3_Click(object sender, EventArgs e)
         {
@@ -89,6 +89,9 @@ namespace AutoLabelme
                    
                 }
                 textBox1.Text = "All picture label finished";
+            }else
+            {
+                Console.WriteLine("The TXT Length is not equal to Image Length");
             }
         }
 
@@ -97,49 +100,102 @@ namespace AutoLabelme
         {
             double scale = 0;
             int distanceToInt = Convert.ToInt32(Math.Ceiling(distance)); ///user to the 2.3.4.5.6m
-            float distanceRemainder = distance - Convert.ToInt32(Math.Floor(distance));
+            //float distanceRemainder = distance - Convert.ToInt32(Math.Floor(distance));
 
             //0?
-            if( distance < 2)
+            if (distanceToInt >= 2 && distanceToInt <=4)
             {
-
-            }
-            else if (distance >=2 && distance < 3)
+                scale = affirScale[distanceToInt - 2];
+            }/*
+            else if (distance >= 2 && distance < 3)
             {
                 // 2m ： 205
                 // 3m :  120
-                scale = (distance - 2.0f) * (205 - 120) / (3.0f - 2.0f);
+                scale = scaleCalculate(distance, 2.0f, 205f, 3f, 120f);
+                //  scale = (distance - 2.0f) * (205 - 120) / (3.0f - 2.0f);
                 //scale = affirScale[distanceToInt - 2];
             }
-            else if (distance >= 3 && distance < 6)
+            else if (distance >= 3 && distance < 4)
             {
-                //
-                scale = affirScale[distanceToInt - 2];
+                scale = scaleCalculate(distance, 3.0f, 120f, 4.0f, 91f);
             }
-            else if (distance >= 6 && distance < 8)
+            else if (distance >= 4 && distance < 6)
             {
-                scale = affirScale[distanceToInt - 2];
+                scale = scaleCalculate(distance, 4.0f, 91f, 6.0f, 55f);
             }
-            else if(distance > 8 && distance < 10)
+            else if (distance >=7.5f && distance < 8)
             {
-                scale = 55 - 10 * distanceRemainder;
+                scale = scaleCalculate(distance, 6.0f, 70f, 8.0f, 41f);
             }
-            else if (distance >= 20 && distance <28)
+             */
+            else if (distance >= 5.0 && distance < 6)
+            {
+                scale = scaleCalculate(distance, 5.0f, 66f, 6.0f, 58f);
+            }
+            else if (distance >= 6.0 && distance < 7)
+            {
+                scale = scaleCalculate(distance, 6.0f, 54f, 7.0f, 50f);
+            }
+             else if (distance >= 7.0 && distance < 8)
+            {
+                scale = scaleCalculate(distance, 7.0f, 44f, 8.0f, 40f);
+            }
+           
+            else if (distance >= 8 && distance < 10)
+            {
+                scale = scaleCalculate(distance, 8.0f, 42f, 10.0f, 35f);
+            }
+            else if (distance >= 10 && distance < 15)
+            {
+                scale = scaleCalculate(distance, 10.0f, 36f, 20.0f, 20f);
+            }
+            else if (distance >= 15 && distance < 20)
+            {
+                scale = scaleCalculate(distance, 15.0f, 20f, 20.0f, 16f);
+            }
+            else if (distance >= 20 && distance < 30)
+            {
+                scale = scaleCalculate(distance, 20.0f, 16f, 30.0f, 10f);
+            }
+            else if (distance >= 30 && distance < 45)
+            {
+                scale = scaleCalculate(distance, 12.0f, 12f, 45.0f, 8f);
+            }
+            else if (distance >= 45 && distance < 60)
+            {
+                scale = scaleCalculate(distance, 45.0f, 10f, 60.0f,8f);
+            }else if(distance >=60  && distance < 70)
+            {
+                scale = 5;
+            }
+
+            /*
+            else if (distance >= 20 && distance < 28)
             {
                 // 20m ： 22
                 int a = 17;
                 // 30m :  15
                 int b = 12;
-                scale = a-(distance - 20.0f) * (a - b) / (28.0f - 20.0f);
+                scale = a - (distance - 20.0f) * (a - b) / (28.0f - 20.0f);
                 //scale = affirScale[distanceToInt - 2];
+            }
+*/
+            else if(distance >= 70 && distance < 80)
+            {
+                scale = 4;
             }
 
             else
             {
-                scale = 6;
+                scale = 3;
             }
             
             return scale;
+        }
+
+        public double scaleCalculate(float distance, float mileMin,float RectMax, float mileMax,float RectMin )
+        {
+            return RectMax - (distance - mileMin) * (RectMax - RectMin) / (mileMax - mileMin);
         }
         //------Draw Rect
         public void  DrawRect(Image image ,Point p0 ,Point p1,string saveImgPath, Point center)
