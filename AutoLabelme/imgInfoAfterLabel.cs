b8226ff0c84585569738d385fb72b25462168826
alisa <alisa.mao@vehicular.ai>
2018-03-28 17:56:57 +0800

    the Class about ImageInfo  After  Label

---
+++ AutoLabelme/imgInfoAfterLabel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+
+
+[XmlRoot("imgInfoAfterLabel", IsNullable = false)]
+public class imgInfoAfterLabel
+{
+    public string labelObj_name;
+    public int bound_left_x;
+    public int bound_right_x;
+    public int bound_left_y;
+    public int bound_right_y;
+
+
+}
